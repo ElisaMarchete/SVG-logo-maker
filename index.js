00065c2f615ac4a8afffa4d5002fa8f88d226148
Joao Marchete <elisamarchete@gmail.com>
2023-05-08 16:27:44 -0400

    prompt and SVG document generated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // loading thitd party modules
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateSVG = require("./lib/generateSVG");
 
 // Function to ask user questions and generate readme.md file
 function getInput() {
@@ -17,11 +18,35 @@ function getInput() {
           return "Please enter up to three characters.";
         },
       },
+      {
+        type: "input",
+        name: "textColor",
+        message:
+          "Select the text's color: Please enter a color keyword OR a hexadecimal number",
+      },
+      {
+        type: "list",
+        name: "shape",
+        message: "'Select the shape",
+        choices: ["circle", "triangle", "square"],
+      },
+      {
+        type: "input",
+        name: "shapeColor",
+        message:
+          "Select the shape's color: Please enter a color keyword OR a hexadecimal number",
+      },
     ])
     .then((answers) => {
       console.log(answers);
 
-      // WriteFile function to create SVG file
+      // generateMarkdown function call with user answers as argument -> function from module generateMarkdown.js
+      const svgFile = generateSVG(answers);
+
+      // WriteFile function to create readme.md file
+      fs.writeFile("examples/logo.svg", svgFile, (err) =>
+        err ? console.log(err) : console.log("Successfully created logo!")
+      );
     });
 }
 
